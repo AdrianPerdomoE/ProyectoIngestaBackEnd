05fa5a577ca222ac64033fcf322f13a8e8d1cebe
Fasbox <63003256+Fasbox@users.noreply.github.com>
2022-11-02 22:18:59 -0500

    Documentación Proyecto Controller

--- controllers/Proyectocontroller.js
+++ controllers/Proyectocontroller.js
@@ -63,12 +63,13 @@ var controller = {
     /**
      * Método para buscar un proyecto por id en la base de datos, devuelve el proyecto encontrado
      * 
-     * @param {String} productoId
+     * @param {String} productoId - Id del proyecto que se desea buscar
      * 
      * @returns {Object} res.status(500).send() - Error para obtener lo que hay en la base de datos y despliega mensaje de error
      * @returns {Object} res.status(404).send() - Error para encontrar el proyecto, se despliega además mensaje de error
      * @returns {Object} res.status(200).send() - Método exitoso y se obtiene la información del proyecto
-     */ {
+     */
+    {
         var proyectoId = req.params.id;
         Proyect.findById(proyectoId, (err, proyecto) => {
             if (err) {
@@ -84,8 +85,11 @@ var controller = {
     /**
      * Método para buscar todos los proyectos que existen en la base de datos, devuelve una lista con todos los elementos
      * 
-     * 
-     */ {
+     * @return {Object} res.status(500).send() - Error para obtener la lista
+     * @return {Object} res.status(404).send() - Error en encontrar la lista con los parámetros dados
+     * @return {Object} res.status(200).send() - Obtiene la lista con los proyectos pedidos
+     */
+    {
         Proyect.find({}).exec((err, proyectos) => {
             if (err) {
                 return res.status(500).send({ msg: "Ha ocurrido un error cargando los proyectos" });
@@ -100,7 +104,13 @@ var controller = {
     /**
      * Método para buscar todos los proyectos de un creador por el id
      * 
-     */ {
+     * @param {String} Id - Id del usuario en cuestión
+     * 
+     * @return {Object} res.status(500).send() - No se pudo cargar la lista de proyectos en la interfaz
+     * @return {Object} res.status(404).send() - La lista con los parámetros a buscar no existen
+     * @return {Object} res.status(200).send() - Carga los proyectos con los parámetros dados
+     */
+    {
         var CreadorId = req.params.id;
         Proyect.find({ creadorId: CreadorId }).exec((err, proyectos) => {
             if (err) {
@@ -116,7 +126,13 @@ var controller = {
     /**
      * Método para buscar todos los proyectos de un tipo de archivo
      * 
-     */ {
+     * @param {String} tipo - Tipo de archivo que se da como parámetro
+     * 
+     * @return {Object} res.status(500).send() - No se encontró el tipo de archivo dado como parámetro
+     * @return {Object} res.status(404).send() - No existen proyectos con los parámetros dados
+     * @return {Object} res.status(200).send() - Se cargan los proyectos con el tipo especificado
+     */
+    {
         let tipo = new RegExp(`${req.params.tipo}`, "i")
         Proyect.find({ tipoArchivo: tipo }).exec((err, proyectos) => {
             if (err) {
@@ -132,7 +148,13 @@ var controller = {
     /**
      * Método para buscar todos los proyectos de un formato
      * 
-     */ {
+     * @param {String} formato - formato que tiene el archivo como parámetro
+     * 
+     * @return {Object} res.status(500).send() - No se encontró el tipo de archivo dado como parámetro
+     * @return {Object} res.status(404).send() - No existen proyectos con los parámetros dados
+     * @return {Object} res.status(200).send() - Se cargan los proyectos con el formato especificado
+     */
+    {
         let formato = new RegExp(`${req.params.formato}`, "i")
         Proyect.find({ formato: formato }).exec((err, proyectos) => {
             if (err) {
@@ -146,9 +168,15 @@ var controller = {
     },
     getProyectosCreador: (req, res) =>
     /**
-     * Método para buscar todos los proyectos de un formato
+     * Método para buscar todos los proyectos de un creador
      * 
-     */ {
+     * @param {String} creador - creador como parámetro para buscar los proyectos de este
+     * 
+     * @return {Object} res.status(500).send() - No se encontró el usuario dado para cargar sus proyectos
+     * @return {Object} res.status(404).send() - Este usuario no tiene proyectos
+     * @return {Object} res.status(200).send() - Se cargan los proyectos del usuario ingresado
+     */
+    {
         let creador = new RegExp(`${req.params.creador}`, "i")
         Proyect.find({ creador: creador }).exec((err, proyectos) => {
             if (err) {
@@ -163,8 +191,15 @@ var controller = {
     getProyectosEtiquetas: (req, res) =>
     /**
      * Método para buscar todos los proyectos por etiquetas
+     * 
+     * @param {JSON} etiquetas - Etiquetas en archivo JSON para filtrado de proyectos
+     * 
+     * @return {Object} res.status(500).send() - No se encontró la etiqueta dada o un error obteniendo los proyectos
+     * @return {Object} res.status(404).send() - No existen proyectos con esa etiqueta
+     * @return {Object} res.status(200).send() - Retorna la lista con las etiquetas dadas
      *
-     */ {
+     */
+    {
         let etiqueta = new RegExp(`${req.params.etiqueta}`, "i")
         Proyect.find({}).exec((err, proyectos) => {
             if (err) {
@@ -182,8 +217,14 @@ var controller = {
      * Método para actualizar un proyecto de la colección, se busca en la base de datos por el id, se actualizan 
      * los valores que se pasan por la peticion y se devuelve el nuevo proyecto con los cambios realizados
      *
+     * @param {Object} id - Id del proyecto en cuestión
+     * 
+     * @return {Object} res.status(500).send() - Error al tratar de actualizar la información del proyecto
+     * @return {Object} res.status(404).send() - No se encontró el proyecto que se busca actualizar
+     * @return {Object} res.status(200).send() - devuelve "Proyecto actualizado" y se actualizan los valores ingresados por el usuario
      *   
-     */ {
+     */
+    {
         var proyectoId = req.params.id;
         var upData = req.body;
         Proyect.findByIdAndUpdate(proyectoId, upData, { new: true }, (err, proyectoActualizado) => {
@@ -200,7 +241,13 @@ var controller = {
     /**
      * Método para eliminar un proyecto de la base de datos, se busca por la id, se devuelve el proyecto eliminado
      * 
-     */ {
+     * @param {Object} id - Id del proyecto en cuestión
+     * 
+     * @return {Object} res.status(500).send() - Error al tratar de eliminar el proyecto
+     * @return {Object} res.status(404).send() - No se encontró el proyecto que se busca eliminar
+     * @return {Object} res.status(200).send() - devuelve "Proyecto eliminado" y se hace el proceso de eliminarlo de la base de datos
+     */
+    {
         var proyectoId = req.params.id;
         Proyect.findByIdAndRemove(proyectoId, (err, proyectoEliminado) => {
             if (err) {
@@ -216,7 +263,13 @@ var controller = {
     /**
      * Método para guardar un archivo en el servidor relacionado con los metodos de crear y actualizar PROYECTO 
      * 
-     */ {
+     * @param {Object} id - Id del proyecto en cuestión
+     * 
+     * @return {Object} res.status(500).send() - Error al tratar de subir el proyecto
+     * @return {Object} res.status(404).send() - Error al tratar de encontrar el archivo que se quiere subir
+     * @return {Object} res.status(200).send() - Sube exitosamente el proyecto
+     */
+    {
         var product_id = req.params.id;
         var NombreArchivo = "Archivo no subido...";
         if (req.files) {
@@ -244,7 +297,11 @@ var controller = {
     /**
      * Método para devolver la ruta del archivo
      * 
-     */ {
+     * @param {Object} archivo - archivo que se busca
+     * 
+     * @return {Object} res.status(200).send() - No se econtró un archivo con esos parámetros
+     */
+    {
         var file = req.params.archivo;
         var path_file = `./archivos/${file}`;
         fs.exists(path_file, (exists) => {
@@ -273,7 +330,13 @@ var controller = {
     /**
      * Método para buscar en la base de datos los proyectos deacuerdo al regular expresion
      * 
-     */ {
+     * @param {Object} searchBy - proyecto que se busca
+     * 
+     * @return {Object} res.status(500).send() - Error cargando los proyectos con la expresión dada
+     * @return {Object} res.status(404).send() - Error al no econtrar proyectos con el parámetro dado
+     * @return {Object} res.status(200).send() - Se cargan los archivos con el parámetro dado
+     */
+    {
 
         let proyectoRegular = new RegExp(`${req.params.searchBy}`, "i")
         Proyect.find({ nombre: proyectoRegular }).exec((err, proyectos) => {
